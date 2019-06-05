c10b54edc2f39e3160069b376e2558803769d77b
Oliver <lidongyue12138@gmail.com>
2019-06-05 22:48:11 +0800

    change run

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import keras_segmentation
+from keras.models import load_model
 import os
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" 
@@ -10,12 +11,13 @@ test_images_path = "./Datasets/CUB_200_2011/test/imgs/"
 test_segs_path = "./Datasets/CUB_200_2011/test/segs"
 
 model = keras_segmentation.models.fcn.fcn_8(n_classes=2 ,  input_height=416, input_width=608  )
+# model.load_weights("./tmp/vgg_unet_1.1")
 
-# model.train( 
-#     train_images =  train_images_path,
-#     train_annotations = train_segs_path,
-#     checkpoints_path = "./tmp/fcn_fcn8_1" , epochs=2
-# )
+model.train( 
+    train_images =  train_images_path,
+    train_annotations = train_segs_path,
+    checkpoints_path = "./tmp/fcn_fcn8_1" , epochs=2
+)
 
 # out = model.predict_segmentation(
 #     inp= os.path.join(images_path, "001.Black_footed_Albatross", "Black_Footed_Albatross_0001_796111.jpg"),
--- utils/DataPreprocess.py
+++ utils/DataPreprocess.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 images_path = "./Datasets/CUB_200_2011/images/"
 segs_path = "./Datasets/CUB_200_2011/segmentations/"
@@ -72,9 +73,45 @@ def make_dataset_CUB():
             # Save segmentations
             img = cv2.imread(os.path.join(CUB_DATA_PATH, "converted", seg_dir))
             cv2.imwrite(os.path.join(CUB_DATA_PATH, "test", "segs", seg_name), img)
-        
+
+def convert_VOC(image):
+    image[image[:, :] == [0, 0, 0]] = 0
+    image[image[:, :] == [0, 0, 128]] = 1
+    image[image[:, :] == [0, 128, 0]] = 2
+    image[image[:, :] == [0, 128, 128]] = 3
+    image[image[:, :] == [128, 0, 0]] = 4
+    image[image[:, :] == [128, 0, 128]] = 5
+    image[image[:, :] == [128, 128, 0]] = 6
+    image[image[:, :] == [128, 128, 128]] = 7
+    image[image[:, :] == [0, 0, 64]] = 8
+    image[image[:, :] == [0, 0, 192]] = 9
+    image[image[:, :] == [0, 128, 64]] = 10
+    image[image[:, :] == [0, 128, 192]] = 11
+    image[image[:, :] == [128, 0, 64]] = 12
+    image[image[:, :] == [128, 0, 192]] = 13
+    image[image[:, :] == [128, 128, 64]] = 14
+    image[image[:, :] == [128, 128, 192]] = 15
+    image[image[:, :] == [0, 64, 0]] = 16
+    image[image[:, :] == [0, 64, 128]] = 17
+    image[image[:, :] == [0, 192, 0]] = 18
+    image[image[:, :] == [0, 192, 128]] = 19
+    image[image[:, :] == [128, 64, 0]] = 20
+    image[image[:, :, 0] > 20] = 0 # Edge as Background
+    return image
+
+
+def convert_segmmentations_VOC():
+    if not os.path.exists(os.path.join(VOC_DATA_PATH, "converted")):
+        os.makedirs(os.path.join(VOC_DATA_PATH, "converted"))
+    for image_dir in os.listdir(os.path.join(VOC_DATA_PATH, "SegmentationClass")):
+        img = cv2.imread(os.path.join(VOC_DATA_PATH, "SegmentationClass", image_dir), 1)
+        img = convert_VOC(img)
+        cv2.imwrite(os.path.join(VOC_DATA_PATH, "converted", image_dir),img)
+
+
 def make_dataset_VOC():
     pass
 
+
 if __name__ == "__main__":
-    make_dataset_CUB()
+    convert_segmmentations_VOC()
