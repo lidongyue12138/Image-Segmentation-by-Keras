67932d92ecee60ea3fec1890f0447c71b853ff83
Oliver <lidongyue12138@gmail.com>
2019-06-06 09:19:18 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,12 @@ train_segs_path = "./Datasets/CUB_200_2011/train/segs/"
 test_images_path = "./Datasets/CUB_200_2011/test/imgs/"
 test_segs_path = "./Datasets/CUB_200_2011/test/segs"
 
-model = keras_segmentation.models.fcn.fcn_8_vgg(n_classes=2 ,  input_height=416, input_width=608)
+# train_images_path = "./Datasets/VOC/train/imgs/"
+# train_segs_path = "./Datasets/VOC/train/segs/"
+# test_images_path = "./Datasets/VOC/test/imgs/"
+# test_segs_path = "./Datasets/VOC/test/segs"
+
+model = keras_segmentation.models.fcn.fcn_32_vgg(n_classes=2 ,  input_height=416, input_width=608)
 # model.load_weights("./tmp/vgg_unet_1.1")
 
 model.train( 
